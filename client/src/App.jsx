2b3eda951fe1f0fac6012e50979fefb9f4efcc3f
SamusNiko <samusevich98@gmail.com>
2021-04-03 15:05:32 +0300

    Basic set up

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <>
+            <div>
+                To Do
+            </div>
+            <div>
+                In Review
+            </div>
+            <div>
+                Done
+            </div>
+        </>
+    )
+}
+
+export default App;
